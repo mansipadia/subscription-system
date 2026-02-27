89ebf0f9918ad1b5330543802b15d095e55d0349
mansipadia <mansi.padia@sugarfit.com>
2026-02-27 12:35:46 +0530

    Add Readme File

--- src/main/java/com/example/subscription/service/PaymentGatewaySimulator.java
+++ src/main/java/com/example/subscription/service/PaymentGatewaySimulator.java
@@ -6,7 +6,7 @@ import java.math.BigDecimal;
 @Service
 public class PaymentGatewaySimulator {
     public boolean process(BigDecimal amount){
-//        return amount.remainder(BigDecimal.valueOf(2)).compareTo(BigDecimal.ZERO) == 0;
-       return false;
+        return amount.remainder(BigDecimal.valueOf(2)).compareTo(BigDecimal.ZERO) == 0;
+//       return false;
     }
 }
